ef6af255f5efde88f45b2decd4fef29ae76d5550
Olivier Calzi <olivier.calzi@calzi.eu>
2025-06-23 08:30:23 +0200

    feat(init): init commit

--- users.tf
+++ users.tf
@@ -1,17 +1,29 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MIT
 
-provider "googleworkspace" {}
+provider "googleworkspace" {
+  customer_id = "123244"
+}
+
+provider "random" {
+  
+}
 
 locals {
   users = csvdecode(file("${path.module}/users.csv"))
+}
+
+ephemeral "random_password" "db_password" {
+  length           = 16
+  override_special = "!#$%&*()-_=+[]{}<>:?"
 }
 
 resource "googleworkspace_user" "users" {
   for_each = { for user in local.users : user.first_name => user }
 
   primary_email = each.value.email
-  password      = each.value.password
+  password_wo = ephemeral.random_password.db_password.result
+  password_wo_version = 1
   hash_function = each.value.password_hash_function
 
   name {
--- versions.tf
+++ versions.tf
@@ -2,11 +2,16 @@
 # SPDX-License-Identifier: MIT
 
 terraform {
-  required_version = "~> 1.0.0"
+#  required_version = "~> 1.0.0"
   required_providers {
     googleworkspace = {
       source = "hashicorp/googleworkspace"
-      version = "0.4.0"
+#      version = "0.7.0"
     }
-  }
+
+    random = {
+      source = "hashicorp/random"
+      version = "3.7.2"
+    }
+ }
 }
